441ef707b04e7753905bef4eb80f85ef17c32194
ravinder sian <rsian761@gmail.com>
2023-12-19 20:25:27 +0000

    add pojos

--- grocery-app/src/main/java/com/personal/model/Product.java
+++ grocery-app/src/main/java/com/personal/model/Product.java
@@ -1,5 +1,17 @@
 package com.personal.model;
 
+import lombok.Data;
+
+@Data
 public class Product {
 
+	private Long id;
+	private String tesco_url;
+	private String sainsbury_url;
+	private String lidl_url;
+	private String home_bargains_url;
+	private String asda_url;
+	private String morrissons_url;
+	private String aldi_url;
+	
 }
--- grocery-app/src/main/java/com/personal/model/Shopping.java
+++ grocery-app/src/main/java/com/personal/model/Shopping.java
@@ -1,5 +1,15 @@
 package com.personal.model;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class Shopping {
 
+	private Long id;
+	private List<Product> products;
+	private BigDecimal totalPrice;
+	
 }
