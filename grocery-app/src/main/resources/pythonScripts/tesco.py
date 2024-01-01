fbb8a38592dbb3abf92e57ff0223da3ce23c0ff3
ravinder sian <rsian761@gmail.com>
2024-01-01 22:06:10 +0000

    script opens page

--- grocery-app/src/main/resources/pythonScripts/tesco.py
+++ grocery-app/src/main/resources/pythonScripts/tesco.py
@@ -1,11 +1,16 @@
 import sys
 from selenium import webdriver
 
-# Create a new instance of the Firefox driver
-driver = webdriver.Chrome()
+#This stops the window closing immediately
+options = webdriver.EdgeOptions()
+options.add_experimental_option("detach", True)
 
-# Open the webpage
-driver.get(sys.argv[1])
+#The below code will open up a chrome browser
+#This tells us we are using a chrome browser
+#We also provide the options so the tab does not close immediately
+web = webdriver.Edge(options=options)
+
+web.get(sys.argv[1])
 
 # Find the element by ID
-element = driver.find_element_by_id("myButton")
+# element = driver.find_element_by_id("myButton")
