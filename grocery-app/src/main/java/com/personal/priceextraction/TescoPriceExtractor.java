157c1d63162d6a489140a083607af4db873bb3f3
ravinder sian <rsian761@gmail.com>
2024-01-05 08:00:06 +0000

    working selenium example

--- grocery-app/src/main/java/com/personal/priceextraction/TescoPriceExtractor.java
+++ grocery-app/src/main/java/com/personal/priceextraction/TescoPriceExtractor.java
@@ -4,16 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TescoPriceExtractor implements CommandLineRunner {
      
 	@Override
 	public void run(String... args) throws Exception {
 		 WebDriver driver = new EdgeDriver();
+		 System.out.println("SELENIUM --------------");
 		 
 	     try {
-	         driver.get("https://www.tesco.com/groceries/en-GB/products/253034146");
-	         driver.findElement(By.xpath("//*[@id=\"asparagus-root\"]/div/div[2]/main/div/div/div[2]/div/section[2]/div[2]/div[1]/div/div/div[1]/p"));
+	         driver.get("https://www.tesco.com/groceries/en-GB/products/261384030");
+	         String price = driver.findElement(By.xpath("//*[@id=\"asparagus-root\"]/div/div[2]/main/div/div/div[2]/div/section[2]/div[2]/div[1]/div/div/div[1]/p")).getText();
+			 System.out.println("SELENIUM --------------" + price);
+
 	     } finally {
 	         driver.quit();
 	     }
