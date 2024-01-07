989fdb338440f8e05d7676096295338ef4886afb
ravinder sian <rsian761@gmail.com>
2024-01-07 22:14:46 +0000

    add clubcard code

--- grocery-app/src/main/java/com/personal/priceextraction/TescoPriceExtractor.java
+++ grocery-app/src/main/java/com/personal/priceextraction/TescoPriceExtractor.java
@@ -14,11 +14,15 @@ public class TescoPriceExtractor implements CommandLineRunner {
 		 WebDriver driver = new EdgeDriver();
 		 System.out.println("SELENIUM --------------");
 		 
+		 //Clubcard div XPath: //*[@id="asparagus-root"]/div/div[2]/main/div/div/div[2]/div/section[2]/div[2]/div[1]/div/a/div/div/div/div/div
+		 
 	     try {
-	         driver.get("https://www.tesco.com/groceries/en-GB/products/261384030");
-	         String price = driver.findElement(By.xpath("//*[@id=\"asparagus-root\"]/div/div[2]/main/div/div/div[2]/div/section[2]/div[2]/div[1]/div/div/div[1]/p")).getText();
+	         driver.get("https://www.tesco.com/groceries/en-GB/products/303776050");
+	         String price = driver.findElement(By.xpath("//*[@id=\"asparagus-root\"]/div/div[2]/main/div/div/div[2]/div/section[2]/div[2]/div[2]/div/div/div[1]/p")).getText();
 			 System.out.println("SELENIUM --------------" + price);
-
+	         String clubcardPrice = driver.findElement(By.xpath("//*[@id=\"asparagus-root\"]/div/div[2]/main/div/div/div[2]/div/section[2]/div[2]/div[1]/div/a/div/div/div/div/div/span")).getText();
+			 System.out.println("SELENIUM --------------" + clubcardPrice);
+			 
 	     } finally {
 	         driver.quit();
 	     }
